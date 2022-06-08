a0d0b85984afe1c5980be55cfa9ff2b69c1975c5
Leandro Novais <90646451+lenovais56@users.noreply.github.com>
2022-06-08 11:07:42 -0300

    atualizando exercício 7-condicionais

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -7,20 +7,25 @@ const listaDeDestinos = new Array(
 );
 
 const idadeComprador = 15;
+const estaAcompanhada = true;
+
 console.log("Destinos disponíveis: ");
 console.log(listaDeDestinos);
 
-if(idadeComprador >= 18){
+if (idadeComprador >= 18) {
     console.log("Comprador maior de idade");
-    listaDeDestinos.splice(1,1);//removendo um item na lista
-}else{
+    listaDeDestinos.splice(1, 1);//removendo um item na lista
+} else if (estaAcompanhada) {
+    console.log("Comprador esta acompanhado");
+    listaDeDestinos.splice(1, 1);//removendo um item na lista
+} else {
     console.log("Não é maior de idade e não posso vender");
 }
 
 console.log(listaDeDestinos);
 
-console.log(idadeComprador > 18);
-console.log(idadeComprador < 18);
-console.log(idadeComprador >= 18);
-console.log(idadeComprador <= 18);
-console.log(idadeComprador == 18);
+//console.log(idadeComprador < 18);
+//console.log(idadeComprador > 18);
+//console.log(idadeComprador <= 18);
+//console.log(idadeComprador >= 18);
+//console.log(idadeComprador == 18);
